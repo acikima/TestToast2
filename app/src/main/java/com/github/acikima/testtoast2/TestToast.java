3a43d3cc4b50a502e71d0092d418a5d24d0107cf
Ima <fatimatul@sage42.net>
2019-05-30 15:55:05 +0800

    empty controctor and add function

--- app/src/main/java/com/github/acikima/testtoast2/TestToast.java
+++ app/src/main/java/com/github/acikima/testtoast2/TestToast.java
@@ -5,22 +5,15 @@ import android.content.Intent;
 import android.widget.Toast;
 
 public class TestToast {
-    String message;
+//    String message;
 
-    public TestToast(String message) {
-        this.message = message;
+    public TestToast() {
+//        this.message = message;
     }
 
-    public String getMessage() {
-        return message;
-    }
 
-    public void setMessage(Context context, String message) {
-        this.message = message;
-        showToast(context);
-    }
 
     public void showToast(Context context){
-        Toast.makeText(context, this.message, Toast.LENGTH_LONG);
+        Toast.makeText(context, "U made it Imaaaa", Toast.LENGTH_LONG);
     }
 }
