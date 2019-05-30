2ad848447646f1a6fa64f0ec5846641ed7d8c2a6
Ima <fatimatul@sage42.net>
2019-05-30 14:12:48 +0800

    empty constructor

--- app/src/main/java/com/github/acikima/testtoast2/TestToast.java
+++ app/src/main/java/com/github/acikima/testtoast2/TestToast.java
@@ -1,26 +1,10 @@
 package com.github.acikima.testtoast2;
 
 import android.content.Context;
+import android.content.Intent;
 import android.widget.Toast;
 
 public class TestToast {
-    String message;
-
-    public TestToast(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(Context context, String message) {
-        this.message = message;
-        showToast(context);
-    }
-
-    public void showToast(Context context){
-
-        Toast.makeText(context, this.message, Toast.LENGTH_SHORT).show();
+    public TestToast() {
     }
 }
