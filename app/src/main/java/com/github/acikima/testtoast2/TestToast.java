25075fee42f94b386bdf499ade505a045fbd8ff6
Ima <fatimatul@sage42.net>
2019-05-31 14:50:21 +0800

    add show dialog function

--- app/src/main/java/com/github/acikima/testtoast2/TestToast.java
+++ app/src/main/java/com/github/acikima/testtoast2/TestToast.java
@@ -1,6 +1,8 @@
 package com.github.acikima.testtoast2;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.widget.Toast;
 
 public class TestToast {
@@ -16,5 +18,12 @@ public class TestToast {
 
     public void showToast(Context context){
         Toast.makeText(context, "U made it Imaaaa", Toast.LENGTH_LONG);
+    }
+
+    public void showDialog(Context context){
+        new AlertDialog.Builder(context)
+                .setTitle("Delete entry")
+                .setMessage("Are you sure you want to delete this entry?")
+                .show();
     }
 }
