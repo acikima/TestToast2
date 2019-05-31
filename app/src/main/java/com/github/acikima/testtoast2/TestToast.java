bb15576bdec1465bb062a8c7d409ab43ad9daa27
Ima <fatimatul@sage42.net>
2019-05-31 12:45:42 +0800

    add application tag in android manifest and add context in java class

--- app/src/main/java/com/github/acikima/testtoast2/TestToast.java
+++ app/src/main/java/com/github/acikima/testtoast2/TestToast.java
@@ -1,21 +1,28 @@
 package com.github.acikima.testtoast2;
 
+import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
-public class TestToast {
+public class TestToast extends Application {
+    private static TestToast mContext;
     String greet;
 
     public TestToast() {
+        mContext = this;
         this.greet = "Hey Ima";
+    }
+
+    public static Context getContext() {
+        return mContext;
     }
 
     public String getGreet() {
         return greet;
     }
 
-    public void showToast(Context context){
-        Toast.makeText(context, "U made it Imaaaa", Toast.LENGTH_LONG);
+    public void showToast(){
+        Toast.makeText(TestToast.getContext(), "U made it Imaaaa", Toast.LENGTH_LONG);
     }
 }
